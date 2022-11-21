93f7aff2d2cb8d095edc2958ecf9a7678a1dd970
hanting.Sheng <AliliToBeNo1>
2022-11-21 17:05:14 +0800

    make myself

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm -rf /usr/local/bin/myBlog/data /usr/local/bin/myBlog/config.toml
-cp -rf exampleSite/config.toml /usr/local/bin/myBlog/
-cp -rf exampleSite/data /usr/local/bin/myBlog/
+cp -rf /usr/local/bin/myBlog/exampleSite/config.toml /usr/local/bin/myBlog/
+cp -rf /usr/local/bin/myBlog/exampleSite/data /usr/local/bin/myBlog/
 cd /usr/local/bin/myBlog/
 hugo
 hugo server --baseUrl=43.139.162.251 --bind=0.0.0.0
