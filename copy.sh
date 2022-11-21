47dc2164caec660395adcce7b4149956f49f3060
hanting.Sheng <AliliToBeNo1>
2022-11-21 17:08:41 +0800

    make myself

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm -rf /usr/local/bin/myBlog/data /usr/local/bin/myBlog/config.toml
-cp -rf /usr/local/bin/myBlog/exampleSite/config.toml /usr/local/bin/myBlog/
-cp -rf /usr/local/bin/myBlog/exampleSite/data /usr/local/bin/myBlog/
+cp -rf /usr/local/bin/myBlog/themes/WebStack-Hugo/exampleSite/config.toml /usr/local/bin/myBlog/
+cp -rf /usr/local/bin/myBlog/themes/WebStack-Hugo/exampleSite/data /usr/local/bin/myBlog/
 cd /usr/local/bin/myBlog/
 hugo
 hugo server --baseUrl=43.139.162.251 --bind=0.0.0.0
